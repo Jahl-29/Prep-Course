0fb3818e3fb2e48243ea23e940b2eefdac022e15
Jahl-29 <higuerey00@gmail.com>
2022-05-03 17:14:20 -0300

    respuestas de JS-2

---
+++ 03-JS-II/homework/respuestas.md/respuestas.js
@@ -0,0 +1,31 @@
+/* # Homework: Javascript II
+
+## Instrucciones
+---
+1. En un archivo de texto separado que debes crear, escribe explicaciones de los siguientes 
+conceptos como si se lo estuvieras explicando a un niño de 12 años. Hacer esto te ayudará a 
+descubrir rápidamente cualquier agujero en tu comprensión.
+
+* `for`
+-se puede llamar "ciclo for" ya que con este codigo podemos generar bucles de manera mas rapida y sensilla 
+con solo implementar 3 pasos 
+
+primero indicamos la variable que queremos utilizar  ejem : "var num1 = 0"
+segundo una condicion ejem : "num1 < 10" esto seria una condicion  
+tercero se indica el aumento de la variable (num1++) ya que puede generar un bucle infinito si no se indica 
+
+
+* `&&`
+- estos serian operadores booleanos significan (y,and) este se utiliza para tener valores booleanos 
+true y folse ...tambien se utilizan para comparar  condiciones  
+
+
+`||`
+-son operadores booleanos significan (or,o) se utilizan para enlazar condiciones 
+
+
+
+`!`
+-son operadores booleanos significa (not, no ) se utilizan para negar la operacion booleana 
+que quiere decir esto si tenemos : true --> false || false --> true de esta manera se negarian 
+*/
